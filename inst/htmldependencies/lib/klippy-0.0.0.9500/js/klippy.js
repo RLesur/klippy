97dec320042759dab07429ea30c3beb40e66fc8f
Romain Lesur <romain.lesur@gmail.com>
2018-02-22 22:52:02 +0100

    prettify code

--- inst/htmldependencies/lib/klippy-0.0.0.9500/js/klippy.js
+++ inst/htmldependencies/lib/klippy-0.0.0.9500/js/klippy.js
@@ -20,30 +20,34 @@ function changeTooltipMessage(element, msg) {
   element.setAttribute('aria-label', msg);
 }
 
-function getUriOcticonClippy(klippyColor, klippyOpacity) {
-  var color;
-  if (klippyColor === 'auto') {
-    var a = document.createElement('a');
-    document.body.appendChild(a);
-    color = window.getComputedStyle(a).getPropertyValue('color');
-    document.body.removeChild(a);
-  } else {
-    color = klippyColor;
-  }
+function getUriOcticonClippy() {
   // The following SVG image is Octicons Clippy (version 6.0.1) (c) GitHub, Inc. - MIT License
   // MIT license: https://github.com/primer/octicons/blob/v6.0.1/LICENSE
   // Website: https://octicons.github.com/
-  var image = '<svg version="1.1" class="octicon" viewBox="0 0 14 16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="' + color + '" opacity="' + klippyOpacity + '" fill-rule="evenodd" d="m2 13h4v1h-4v-1zm5-6h-5v1h5v-1zm2 3v-2l-3 3 3 3v-2h5v-2h-5zm-4.5-1h-2.5v1h2.5v-1zm-2.5 3h2.5v-1h-2.5v1zm9 1h1v2c-0.02 0.28-0.11 0.52-0.3 0.7s-0.42 0.28-0.7 0.3h-10c-0.55 0-1-0.45-1-1v-11c0-0.55 0.45-1 1-1h3c0-1.11 0.89-2 2-2s2 0.89 2 2h3c0.55 0 1 0.45 1 1v5h-1v-3h-10v9h10v-2zm-9-8h8c0-0.55-0.45-1-1-1h-1c-0.55 0-1-0.45-1-1s-0.45-1-1-1-1 0.45-1 1-0.45 1-1 1h-1c-0.55 0-1 0.45-1 1z"/></svg>';
-  //var binaryImage = window.btoa(image);
-  //var uriImage = 'data:image/svg+xml;base64,' + binaryImage;
-  //return uriImage;
+  var image = '<svg class="octicon" viewBox="0 0 14 16"><path fill-rule="evenodd" d="m2 13h4v1h-4v-1zm5-6h-5v1h5v-1zm2 3v-2l-3 3 3 3v-2h5v-2h-5zm-4.5-1h-2.5v1h2.5v-1zm-2.5 3h2.5v-1h-2.5v1zm9 1h1v2c-0.02 0.28-0.11 0.52-0.3 0.7s-0.42 0.28-0.7 0.3h-10c-0.55 0-1-0.45-1-1v-11c0-0.55 0.45-1 1-1h3c0-1.11 0.89-2 2-2s2 0.89 2 2h3c0.55 0 1 0.45 1 1v5h-1v-3h-10v9h10v-2zm-9-8h8c0-0.55-0.45-1-1-1h-1c-0.55 0-1-0.45-1-1s-0.45-1-1-1-1 0.45-1 1-0.45 1-1 1h-1c-0.55 0-1 0.45-1 1z"/></svg>';
   return image;
+}
+
+function getAnchorColor() {
+  var a = document.createElement('a');
+  document.body.appendChild(a);
+  var color = window.getComputedStyle(a).getPropertyValue('color');
+  document.body.removeChild(a);
+  return color;
 }
 
 function initKlippy(handSide, headSide, klippyColor, klippyOpacity, tooltipLabel, tooltipLabelSucceed) {
   document.addEventListener("DOMContentLoaded", function() {
-    var image = getUriOcticonClippy(klippyColor, klippyOpacity);
+    var image = getUriOcticonClippy();
     var klippyButton = "<button type='button' class='btn-klippy tooltipped tooltipped-no-delay' aria-label='" + tooltipLabel + "' onfocusout='changeTooltipMessage(this,&quot;" + tooltipLabel + "&quot;)' data-clipboard-klippy>"+ image +"</button>";
+
+    // Process icons color:
+    var color;
+    if (klippyColor === 'auto') {
+      color = getAnchorColor();
+    } else {
+      color = klippyColor;
+    }
 
     // Insert klippy buttons:
     var codeNodeList = document.querySelectorAll(".klippy > code");
@@ -62,6 +66,8 @@ function initKlippy(handSide, headSide, klippyColor, klippyOpacity, tooltipLabel
       var iconHeight = iconHeightWidthRatio * parseFloat(paddingParent) + 'px';
       icon.setAttribute('width', paddingParent);
       icon.setAttribute('height', iconHeight);
+      icon.setAttribute('fill', color);
+      icon.setAttribute('opacity', klippyOpacity);
       icon.style.verticalAlign = headSide;
       if (handSide === 'right') {
         klippy.style.right = '0';
